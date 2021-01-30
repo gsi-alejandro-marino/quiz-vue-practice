47c91c40c9236b822324a3bcc6a6274ff4b863be
gsi-alejandro-marino <alejandro.marino@generalsoftwareinc.com>
2021-01-29 20:35:04 -0500

    feat: question box component and new dep

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div tabs>
+    <b-nav>
+      <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+      <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal}}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    numCorrect: Number,
+    numTotal: Number
+  }
+}
+</script>
